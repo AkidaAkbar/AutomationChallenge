bc82d6613db70ee5b207dfcb14a0200021e1fe37
akidaakbar <aikede22@gmail.com>
2021-03-06 18:10:29 -0500

    Some updates

--- src/main/java/test/HomePageTest.java
+++ src/main/java/test/HomePageTest.java
@@ -1,4 +1,15 @@
 package test;
 
+import org.junit.jupiter.api.Test;
+import utils.Driver;
+
+
 public class HomePageTest {
+
+    @Test
+    public void Test1() {
+        Driver.getDriver().get("file:///Users/akidaakbar/Desktop/AutomationChallenge-2020/index.html#");
+
+
+    }
 }
