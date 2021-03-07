57d81dd061bde0d5d2fab72432d77ee702e173e4
akidaakbar <aikede22@gmail.com>
2021-03-06 21:25:03 -0500

    New updates

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -1,4 +1,88 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import utils.Driver;
+
+
+import static org.openqa.selenium.By.*;
+
 public class HomePage {
+    private static final By EMAIL_ADDRESS_INPUT = id("inputEmail");
+    private static final By PASSWORD_INPUT = id("inputPassword");
+    private static final By SIGN_IN_BUTTON = xpath("//button[@type='submit']");
+    private static final By VALUE_ONE = xpath("//*[@id='test-2-div']/ul/li[1]");
+    private static final By VALUE_TWO = xpath("//*[@id='test-2-div']/ul/li[2]");
+    private static final By VALUE_THREE = xpath("//*[@id='test-2-div']/ul/li[3]");
+    private static final By SECOND_BADGE_VALUE = xpath("//*[@id='test-2-div']/ul/li[2]/span");
+    private static final By DROPDOWN_LIST = xpath("//*[@id='dropdownMenuButton']");
+    private static final By OPTION_THREE = xpath("//*[@id='test-3-div']/div/div/a[3]");
+    private static final By BUTTON_ENABLED= xpath("//button[@class='btn btn-lg btn-primary']");
+    private static final By BUTTON_DISABLED = xpath("//button[@class='btn btn-lg btn-secondary']");
+    private static final By BUTTON= xpath("//*[@id='test5-button']");
+    private static final By SUCCESS_MESSAGE= xpath("//*[@id='test5-alert']");
+
+    public void enterEmail(String email){
+        Driver.getDriver().findElement(EMAIL_ADDRESS_INPUT).sendKeys(email);
+    }
+
+    public void enterPassword(String password){
+        Driver.getDriver().findElement(PASSWORD_INPUT).sendKeys(password);
+    }
+
+    public void clickSignIn(){
+        Driver.getDriver().findElement(SIGN_IN_BUTTON).click();
+    }
+
+    public WebElement getEmailAddressInput(){
+        return Driver.getDriver().findElement(EMAIL_ADDRESS_INPUT);
+    }
+
+    public WebElement getPasswordInput(){
+        return Driver.getDriver().findElement(PASSWORD_INPUT);
+    }
+
+    public WebElement getSignInButton(){
+        return Driver.getDriver().findElement(SIGN_IN_BUTTON);
+    }
+    public WebElement getThreeValues(){
+        return Driver.getDriver().findElement(VALUE_ONE).findElement(VALUE_TWO).findElement(VALUE_THREE);
+    }
+
+    public WebElement getSecondItem(){
+        return Driver.getDriver().findElement(VALUE_TWO);
+    }
+
+    public WebElement getSecondBadgeValue(){
+        return Driver.getDriver().findElement(SECOND_BADGE_VALUE);
+    }
+
+    public WebElement getDefaultValue(){
+        return Driver.getDriver().findElement(DROPDOWN_LIST);
+    }
+
+    public void clickOptionThree(){
+        Driver.getDriver().findElement(DROPDOWN_LIST).click();
+        Driver.getDriver().findElement(OPTION_THREE).click();
+    }
+    public WebElement getFirstButton(){
+        return Driver.getDriver().findElement(BUTTON_ENABLED);
+    }
+
+    public WebElement getSecondButton(){
+        return Driver.getDriver().findElement(BUTTON_DISABLED);
+    }
+    public void clickButton() {
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 10);
+        WebElement element = wait.until(ExpectedConditions.elementToBeClickable(BUTTON));
+        element.click();
+    }
+    public WebElement getSuccessMessage() {
+        return Driver.getDriver().findElement(SUCCESS_MESSAGE);
+    }
+    public WebElement verifyDisabledButton() {
+        return Driver.getDriver().findElement(BUTTON);
+    }
 }
--- src/main/java/test/HomePageTest.java
+++ src/main/java/test/HomePageTest.java
@@ -1,15 +1,66 @@
 package test;
 
-import org.junit.jupiter.api.Test;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import pages.HomePage;
 import utils.Driver;
 
 
 public class HomePageTest {
-
+    HomePage homePage = new HomePage();
+    @Before
+    public void setUp() {
+        Driver.getDriver().get("file:///Users/akidaakbar/Desktop/AutomationChallenge-2020/index.html#");
+    }
     @Test
     public void Test1() {
-        Driver.getDriver().get("file:///Users/akidaakbar/Desktop/AutomationChallenge-2020/index.html#");
+        Assert.assertTrue("Validate that email address input is displayed.",homePage.getEmailAddressInput().isDisplayed());
+        Assert.assertTrue("Validate that password input is displayed.",homePage.getPasswordInput().isDisplayed());
+        Assert.assertTrue("Validate that sign in button is displayed.",homePage.getSignInButton().isDisplayed());
 
+        homePage.enterEmail("aikede22@gmail.com");
+        homePage.enterPassword("Hello1129!");
+        homePage.clickSignIn();
 
     }
+    @Test
+    public void Test2(){
+        Assert.assertTrue("Validate that there are three values in the list group.",homePage.getThreeValues().isDisplayed());
+        Assert.assertTrue("Validate that the second list item's value is set to List Item 2.",homePage.getSecondItem().isDisplayed());
+        Assert.assertTrue("Validate that the second list item's badge value is 6.",homePage.getSecondBadgeValue().isDisplayed());
+    }
+    @Test
+    public void Test3(){
+        Assert.assertTrue("Validate that Option 1 is the default selected value.",homePage.getDefaultValue().isDisplayed());
+        homePage.clickOptionThree();
+    }
+
+    @Test
+    public void Test4(){
+        Assert.assertTrue("Validate that the first button is enabled.",homePage.getFirstButton().isDisplayed());
+        Assert.assertTrue("Validate that the second button is disabled.",homePage.getSecondButton().isDisplayed());
+
+    }
+    @Test
+    public void Test5(){
+        homePage.clickButton();
+        Assert.assertTrue("Validate that the success message has appeared.",homePage.getSuccessMessage().isDisplayed());
+        Assert.assertTrue("Validate that the button is disabled.",homePage.verifyDisabledButton().isDisplayed());
+    }
+
+    @Test
+    public void Test6(){
+
+    }
+
+    @After
+    public void tearDown(){
+        Driver.getDriver().close();
+        Driver.getDriver().quit();
+    }
+
+
 }
